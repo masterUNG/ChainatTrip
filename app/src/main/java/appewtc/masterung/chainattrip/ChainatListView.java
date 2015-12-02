3b3fd362470db8eef3a65d1c14a628dde04f922e
masterUNG <phrombutr@gmail.com>
2015-12-02 13:03:54 +0700

    Create ListView

---
+++ app/src/main/java/appewtc/masterung/chainattrip/ChainatListView.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.chainattrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChainatListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chainat_list_view);
+    }
+}
