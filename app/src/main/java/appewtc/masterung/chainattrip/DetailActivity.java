c33f474ef44832c140fa45f093185fdddf07060a
masterUNG <phrombutr@gmail.com>
2015-12-04 10:58:37 +0700

    Show View (TextView) finish

--- app/src/main/java/appewtc/masterung/chainattrip/DetailActivity.java
+++ app/src/main/java/appewtc/masterung/chainattrip/DetailActivity.java
@@ -2,12 +2,60 @@ package appewtc.masterung.chainattrip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView firstImageView, secondImageView, thirdImageView;
+    private String titleString, firstString, secondString, thirdString, detailString,
+            latString, lngString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //Bind Widget
+        bindWidget();
+
+        //Receive Value From Intent
+        receiveValue();
+
+        //Show View
+        showView();
+
+    }   // Main Method
+
+    private void showView() {
+
+        titleTextView.setText(titleString);
+        detailTextView.setText(detailString);
+
     }
-}
+
+    private void receiveValue() {
+
+        titleString = getIntent().getStringExtra("Title");
+        firstString = getIntent().getStringExtra("Image1");
+        secondString = getIntent().getStringExtra("Image2");
+        thirdString = getIntent().getStringExtra("Image3");
+        detailString = getIntent().getStringExtra("Detail");
+        latString = getIntent().getStringExtra("Lat");
+        lngString = getIntent().getStringExtra("Lng");
+
+    }
+
+    private void bindWidget() {
+
+        titleTextView = (TextView) findViewById(R.id.textView8);
+        detailTextView = (TextView) findViewById(R.id.textView9);
+        firstImageView = (ImageView) findViewById(R.id.imageView6);
+        secondImageView = (ImageView) findViewById(R.id.imageView7);
+        thirdImageView = (ImageView) findViewById(R.id.imageView8);
+
+    }
+
+}   // Main Class
